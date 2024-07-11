d2f64d692927481fc1bd0fa0f8c6669f2f1fc29e
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-07-11 16:27:53 +0530

    added BadCredentials & Catch All exception handler to DefaultExceptionHandler

--- backend/src/main/java/com/chamika/books_project/exceptions/DefaultExceptionHandler.java
+++ backend/src/main/java/com/chamika/books_project/exceptions/DefaultExceptionHandler.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -62,6 +63,41 @@ public class DefaultExceptionHandler {
         );
         return new ResponseEntity<>(apiError, HttpStatus.CONFLICT);
 
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<APIError> handleException(
+            BadCredentialsException e,
+            HttpServletRequest request
+    ) {
+        log.error("Bad Credentials provided ", e);
+
+        APIError apiError = new APIError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.UNAUTHORIZED.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.UNAUTHORIZED);
+    }
+
+    // * Catch all exceptions handler
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIError> handleException(
+            Exception e,
+            HttpServletRequest request
+    ) {
+        log.error("Unhandled Exception Occurred ", e);
+
+        APIError apiError = new APIError(
+                request.getRequestURI(),
+                e.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                LocalDateTime.now()
+        );
+
+        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
