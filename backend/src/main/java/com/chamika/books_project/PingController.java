0c07c8665182f6428ea0bfb4ab4b6281f8e0a37c
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-09-03 15:37:05 +0530

    added a pong controller to check the api health

---
+++ backend/src/main/java/com/chamika/books_project/PingController.java
@@ -0,0 +1,17 @@
+package com.chamika.books_project;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+
+    @GetMapping
+    public String sayPong() {
+        return "I love DevOps. Pong Pong Pong";
+    }
+
+}
